9055882b68c7882b79ef71dd0d46679182f51c31
Ahmed laghrissi <99613580+laghri@users.noreply.github.com>
2023-08-24 10:00:11 +0100

    step3_training change

--- step3_training.py
+++ step3_training.py
@@ -14,7 +14,7 @@ import pandas as pd
 if __name__ == '__main__':
 
     torch.cuda.set_device(utils.get_avail_gpu()) # assign which gpu will be used (only linux works)
-    use_visdom = True # if you don't use visdom, please set to False
+    use_visdom = False # if you don't use visdom, please set to False
 
     train_list = './train_list_1.csv' # use 1-fold as example
     val_list = './val_list_1.csv' # use 1-fold as example
@@ -25,10 +25,10 @@ if __name__ == '__main__':
 
     num_classes = 15
     num_channels = 15 #number of features
-    num_epochs = 200
+    num_epochs = 100
     num_workers = 0
-    train_batch_size = 10
-    val_batch_size = 10
+    train_batch_size = 3
+    val_batch_size = 3
     num_batches_to_print = 20
 
     if use_visdom:

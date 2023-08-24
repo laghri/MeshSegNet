5cef08033a6296c6653983c3c736222edb5f2098
Ahmed laghrissi <99613580+laghri@users.noreply.github.com>
2023-08-24 09:58:54 +0100

    step1 change

--- step1_augmentation.py
+++ step1_augmentation.py
@@ -44,14 +44,14 @@ def GetVTKTransformationMatrix(rotate_X=[-180, 180], rotate_Y=[-180, 180], rotat
 
 if __name__ == "__main__":
 
-    num_samples = 36 # need to define # of samples; e.g., 30
-    vtk_path = 'src' # need to define the path; e.g., src
+    num_samples = 20 # need to define # of samples; e.g., 30
+    vtk_path = 'dataset_dental_VTP/vtp' # need to define the path; e.g., src
     output_save_path = './augmentation_vtk_data'
     if not os.path.exists(output_save_path):
         os.mkdir(output_save_path)
 
     sample_list = list(range(1, num_samples+1))
-    num_augmentations = 20
+    num_augmentations = 5
 
     for i_sample in sample_list:
         for i_aug in range(num_augmentations):
@@ -63,15 +63,15 @@ if __name__ == "__main__":
                                                   scale_X=[0.8, 1.2], scale_Y=[0.8, 1.2], scale_Z=[0.8, 1.2]) #use default random setting
             mesh = load(os.path.join(vtk_path, file_name))
             mesh.applyTransform(vtk_matrix)
-            io.write(mesh, os.path.join(output_save_path, output_file_name))
+            write(mesh, os.path.join(output_save_path, output_file_name))
 
         #flipped mesh
         for i_aug in range(num_augmentations):
-            file_name = 'Sample_0{0}_d.vtp'.format(i_sample+1000)
-            output_file_name = 'A{0}_Sample_0{1}_d.vtp'.format(i_aug, i_sample+1000)
+            file_name = 'Sample_0{0}_d.vtp'.format(i_sample)
+            output_file_name = 'A{0}_Sample_0{1}_d.vtp'.format(i_aug +1000, i_sample)
             vtk_matrix = GetVTKTransformationMatrix(rotate_X=[-180, 180], rotate_Y=[-180, 180], rotate_Z=[-180, 180],
                                                   translate_X=[-10, 10], translate_Y=[-10, 10], translate_Z=[-10, 10],
                                                   scale_X=[0.8, 1.2], scale_Y=[0.8, 1.2], scale_Z=[0.8, 1.2]) #use default random setting
             mesh = load(os.path.join(vtk_path, file_name))
             mesh.applyTransform(vtk_matrix)
-            io.write(mesh, os.path.join(output_save_path, output_file_name))
+            write(mesh, os.path.join(output_save_path, output_file_name))
